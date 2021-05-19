d31dac61edca6a6c575e1bac27ba09548f716b25
atty22 <attilio2.baratta@gmail.com>
2021-05-19 18:47:18 +0200

    fixed

--- Main.py
+++ Main.py
@@ -4,11 +4,15 @@ from os import chdir, path, rename, remove
 from subprocess import run
 from requests import post
 from telegraph import Telegraph
+from platform import system
 
 print('''
 Unlimited Rulebook.
 
-This program need imagick, you can download it from : \
+This program need imagick, you can download it from :
+Arch:  sudo pacman -S imagemagick
+Debian: sudo apt install imagemagick
+Windows:
 https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-12-Q16-HDRI-x64-dll.exe
 
 This program will convert all your png or jpeg image to jpg before upload it on telegraph
@@ -29,21 +33,35 @@ title = ""
 while len(title) == 0:
     title = input("Title: ")
 
-chdir(input("Path: "))
+chdir(input('Folder: ').replace("\\", "\\").replace('"', '').replace("'", "\'"))
 
 # define variable
 nlist = []
 listo = []
 nfilework = 0
-html = "<p> </p>"  # you can add text between > < to display it at the top
+html = '''<p>{}</p>
+'''.format(input("Initial Text:"))
+edtext = '''<p>{}</p>
+'''.format(input("Ending Text:"))
 ordfile = []
 telegraph = Telegraph()
 telegraph.create_account(short_name="Yotsugi")
 
 # convert png to jpg, and rename jpeg to jpg
-for i in glob("*.png"):
-    run(str("magick.exe mogrify -format jpg  -quality 89 " + i), shell=True)
-    remove(i)
+print("Converting file")
+if system() == 'Windows':
+    for i in glob("*.png"):
+        print("Converting: ", i)
+        run(str("magick.exe mogrify -format jpg  -quality 89 " + i), shell=True)
+        remove(i)
+elif system() == 'Linux':
+    for i in glob("*.png"):
+        print("Converting: ", i)
+        run(str("mogrify -format jpg  -quality 89 " + i), shell=True)
+        remove(i)
+else:
+    print("I'm not compatible with your system")
+
 for i in glob("*.jpeg"):
     rename(i, str(path.splitext(i)[0] + ".jpg"))
 
@@ -54,9 +72,8 @@ print("common prefix: ", prefix)
 number0 = len(str(path.splitext(listfile[0])[0][len(prefix):]))
 for item in listfile:
     nlist.append(str(path.splitext(item)[0][len(prefix):]))  # convert str file name in int to sort
-ordlist = [int(x) for x in nlist]
-for itemd in ordlist:
-    itemd = str(itemd)
+nlist.sort(key=int)
+for itemd in nlist:
     while len(itemd) != number0:
         itemd = "0" + itemd
     file = str(prefix + itemd + ".jpg")  # recreate original file name
@@ -64,9 +81,9 @@ for itemd in ordlist:
         html += "<img src='{}'/>".format(post('https://telegra.ph/upload',
                                               files={'file': ('file', f, 'image/jpeg')}).json()[0]['src'])
     nfilework += 1  # for cli GUI
-    print(file, "    Uploaded    ", nfilework, "/", len(ordlist), "    ", int(nfilework / len(ordlist) * 100), "%")
+    print(file, "    Uploaded    ", nfilework, "/", len(nlist), "    ", int(nfilework / len(nlist) * 100), "%")
     # for cli GUI
-html += "<p> </p>"  # you can add text between > < to display it at the end
+html += edtext
 html += "<p>Upload with Yotsugi by @atty2</p>"
 response = telegraph.create_page(title, html_content=html)
 print('''
